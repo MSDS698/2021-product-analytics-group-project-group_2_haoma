3c26bcbb3fd788699aecbe24cf169be6e4991a48
ZachDougherty <zachougherty@gmail.com>
2021-06-03 23:51:05 -0400

    revert back to original structure

--- code/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-import os
-
-application = Flask(__name__)
-application.secret_key = os.urandom(24)
-
-from app import routes
--- code/app/routes.py
+++
@@ -1,8 +0,0 @@
-from app import application
-from flask import render_template
-
-@application.route('/')
-@application.route('/index')
-def index():
-	"Home page for our application."
-	return render_template('index.html')
