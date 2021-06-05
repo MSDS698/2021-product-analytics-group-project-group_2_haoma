e94dc11992bddc860571d6a5526faeff36cab1da
Christabellecp <christabellepabalan@gmail.com>
2021-06-05 08:12:40 -0700

    removed dt import

--- code/app/funcs.py
+++ code/app/funcs.py
@@ -5,7 +5,6 @@ separate from routes to improve
 readability and help with debugging.
 """
 import pandas as pd
-import datatable as dt
 
 HH_DATA_URL = "https://data.cms.gov/provider-data/sites/default/files/resources/1ee6a6e80907bf13661aa2f099415fcd_1620794404/HH_Provider_Oct2020.csv"
 
