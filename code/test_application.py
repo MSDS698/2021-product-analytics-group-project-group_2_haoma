4cecd9819faf285a7aff1ce6491a2694e86ff738
Christabellecp <christabellepabalan@gmail.com>
2021-06-05 08:10:27 -0700

    modify test file

--- code/test_application.py
+++ code/test_application.py
@@ -6,10 +6,11 @@ import numpy as np
 
 def test_zipcode_matches():
 	"Ensure that the zipcode function gets the correct list of Home Health agencies."
-	assert all(get_hh_agencies('36609') == np.array([
+	assert all(get_hh_agencies('36609')['Provider Name'] == np.array([
 		'AMEDISYS HOME HEALTH CARE','SAAD HEALTHCARE','INFIRMARY HOMECARE',
 		'ENCOMPASS HEALTH HOME HEALTH','KINDRED AT HOME'
 	]))
-	assert all(get_hh_agencies('92543') == np.array(['HEMET HOME HEALTH CARE']))
-	assert all(get_hh_agencies('99503') == np.array(['FRONTIER HOME HEALTH','MAXIM HEALTHCARE SERVICES, INC']))
-	assert all(get_hh_agencies('94118') == np.array(['KAISER FOUNDATION HOSP HOME HEALTH - SAN FRANCISCO']))
+	assert all(get_hh_agencies('92543')['Provider Name'] == np.array(['HEMET HOME HEALTH CARE']))
+	assert all(get_hh_agencies('99503')['Provider Name'] == np.array(['FRONTIER HOME HEALTH',
+                                                                      'MAXIM HEALTHCARE SERVICES, INC']))
+	assert all(get_hh_agencies('94118')['Provider Name'] == np.array(['KAISER FOUNDATION HOSP HOME HEALTH - SAN FRANCISCO']))
