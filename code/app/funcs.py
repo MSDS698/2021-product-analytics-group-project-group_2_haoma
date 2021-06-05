7eaa4645b3e1b40130d314a582b49205fbbea39b
Christabellecp <christabellepabalan@gmail.com>
2021-06-05 01:40:10 -0700

    returned df instead of series

--- code/app/funcs.py
+++ code/app/funcs.py
@@ -5,13 +5,13 @@ separate from routes to improve
 readability and help with debugging.
 """
 import pandas as pd
+import datatable as dt
 
 HH_DATA_URL = "https://data.cms.gov/provider-data/sites/default/files/resources/1ee6a6e80907bf13661aa2f099415fcd_1620794404/HH_Provider_Oct2020.csv"
 
 
 def get_hh_agencies(zipcode: str):
-	"Get a pd.Series of Home Health agencies from given zipcode."
+	"Get a df of Home Health agencies from given zipcode."
 	df = pd.read_csv(HH_DATA_URL)
-	hh_data = df.loc[df.ZIP == int(zipcode), 'Provider Name']
-
+	hh_data = df[df.ZIP == int(zipcode)][['Provider Name', 'Address', 'City', 'Phone']]
 	return hh_data
