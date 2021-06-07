18af2fe3120b9a1cc1361046386d4e841aeb0508
ppoon23 <49705846+ppoon23@users.noreply.github.com>
2021-06-06 21:56:28 -0700

    hyperlink fix on zipcode

--- code/app/routes.py
+++ code/app/routes.py
@@ -6,30 +6,33 @@ from wtforms.validators import DataRequired, NumberRange
 
 
 class StringForm(FlaskForm):
-	"Class for a string field form"
-	field = StringField('name', validators=[DataRequired()])
+    "Class for a string field form"
+    field = StringField('name', validators=[DataRequired()])
+
 
 class MiniZipForm(FlaskForm):
-	"Form for zipcode input displayed in index.html"
-	field = IntegerField(label="Zipcode")
+    "Form for zipcode input displayed in index.html"
+    field = IntegerField(label="Zipcode")
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	"Home Page."
-	return render_template('index.html')
+    "Home Page."
+    return render_template('index.html')
 
-@app.route('/zipcode_search', methods=['GET','POST'])
+
+@app.route('/zipcode_search', methods=['GET', 'POST'])
 def zipcode_search():
-	"Page for finding Home Health agencies in given zipcode."
-	form = StringForm()
-	if form.validate_on_submit():
-		zipcode = form.data['field']
-		df = funcs.get_hh_agencies(zipcode)
+    "Page for finding Home Health agencies in given zipcode."
+    form = StringForm()
+    if form.validate_on_submit():
+        zipcode = form.data['field']
+        df = funcs.get_hh_agencies(zipcode)
 
-		return render_template('zipcode.html', 
-                               form=form, 
-                               zipcode=zipcode, 
-                               entries=[df.to_html()])
+        return render_template('zipcode.html',
+                               form=form,
+                               zipcode=zipcode,
+                               entries=[df.to_html(classes=["table-hover-center"])])
 
-	return render_template('zipcode.html', form=form)
+    return render_template('zipcode.html', form=form)
