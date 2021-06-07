715d194900d97679e4ffe36440d8f7204c914faa
GrantEPhillips <gephillips@dons.usfca.edu>
2021-06-06 18:38:07 -0700

    Fix zipcode lookup to find agencies that will service a given zipcode

--- code/app/funcs.py
+++ code/app/funcs.py
@@ -6,11 +6,16 @@ readability and help with debugging.
 """
 import pandas as pd
 
+HH_ZIP_DATA_URL = "https://data.cms.gov/provider-data/sites/default/files/resources/35759790ad0a207f47ba2a079eb51a0f_1620794404/HH_Zip_Oct2020.csv"
+
 HH_DATA_URL = "https://data.cms.gov/provider-data/sites/default/files/resources/1ee6a6e80907bf13661aa2f099415fcd_1620794404/HH_Provider_Oct2020.csv"
 
 
 def get_hh_agencies(zipcode: str):
-	"Get a df of Home Health agencies from given zipcode."
-	df = pd.read_csv(HH_DATA_URL)
-	hh_data = df[df.ZIP == int(zipcode)][['Provider Name', 'Address', 'City', 'Phone']]
-	return hh_data
+    "Get a df of Home Health agencies from given zipcode."
+    df_zip = pd.read_csv(HH_ZIP_DATA_URL)
+    df_data = pd.read_csv(HH_DATA_URL)
+    cms_nums = df_zip[df_zip[' ZIP Code'] == int(zipcode)]['CMS Certification Number (CCN)']
+    hh_data = df_data[df_data['CMS Certification Number (CCN)']
+    .isin(cms_nums)][['Provider Name', 'Address', 'City', 'Phone']]
+    return hh_data
