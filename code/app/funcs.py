0d43fe9dc474726c77443d7c6bd87a31da52b4d2
ZachDougherty <zachougherty@gmail.com>
2021-06-09 14:26:40 -0400

    add geocode func. still need to fix env for gcp api key and regex addresses

--- code/app/funcs.py
+++ code/app/funcs.py
@@ -5,16 +5,22 @@ separate from routes to improve
 readability and help with debugging.
 """
 import pandas as pd
+from urllib.parse import urlencode
+import requests
+import os
+
 
 # Contains xipcodes that HH agencies cover
 HH_ZIP_DATA_URL = "https://data.cms.gov/provider-data/sites/default/files/" \
-                  "resources/35759790ad0a207f47ba2a079eb51a0f_1620794404/" \
+    "resources/35759790ad0a207f47ba2a079eb51a0f_1620794404/" \
                   "HH_Zip_Oct2020.csv"
 
 # Contains information on HH agencies
 HH_DATA_URL = "https://data.cms.gov/provider-data/sites/default/files/" \
               "resources/1ee6a6e80907bf13661aa2f099415fcd_1620794404/" \
               "HH_Provider_Oct2020.csv"
+
+GEOCODE_ENDPOINT = "https://maps.googleapis.com/maps/api/geocode/json?"
 
 
 def get_hh_agencies(zipcode: str):
@@ -30,3 +36,16 @@ def get_hh_agencies(zipcode: str):
                                         'City',
                                         'Phone']]
     return hh_data
+
+def geocode_address(address: str="1 Waterford Ct Ringwood NJ 07546") -> dict:
+    "Return a tuple of lon, lat coordinates for a given address."
+    params = {
+        'address': address, 'key': os.environ['HAOMA_GCP_API_KEY']
+    }
+
+    url = GEOCODE_ENDPOINT + urlencode(params)
+    res = requests.get(url)
+    res_dict = res.json()
+    lat_lon = res_dict['results'][0]['geometry']['location']
+
+    return lat_lon
