631d0a5d113f973477c6a8e574c54ea2f8878353
ZachDougherty <zachougherty@gmail.com>
2021-06-11 13:11:30 -0400

    added sql query for loading zipcode table

---
+++ code/app/db/create_hhcare_zipcodes.sql
@@ -0,0 +1,15 @@
+-- CREATE EXTENSION aws_s3 CASCADE;
+
+DROP TABLE IF EXISTS hhcare_zipcodes;
+
+CREATE TABLE hhcare_zipcodes (
+	state CHAR(2) NOT NULL,
+	cms_certification_number INT NOT NULL,
+	zip_code INT NOT NULL
+);
+
+SELECT aws_s3.table_import_from_s3(
+	'hhcare_zipcodes', 'state,cms_certification_number,zip_code',
+	'(FORMAT csv, HEADER true)',
+	aws_commons.create_s3_uri('haoma-bucket','HH_Zip_Oct2020.csv','us-west-2')
+);
