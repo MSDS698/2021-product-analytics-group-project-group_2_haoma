5e9b635954a1a50b01e8c865c729be639a488610
Noah Matsuyoshi <nmats@uw.edu>
2021-06-10 20:05:21 -0700

    fix deploy script key name

--- eb_deploy.sh
+++ eb_deploy.sh
@@ -1,2 +1,2 @@
-eb init group-2-haoma-app -p Docker -r us-west-2 -k $PEM_NAME
+eb init group-2-haoma-app -p Docker -r us-west-2 --keyname=$PEM_NAME
 eb create group-2-haoma-env --envvar AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvar AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
