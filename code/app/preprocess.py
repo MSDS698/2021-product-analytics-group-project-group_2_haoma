4d5e5779776f8bf5de8993c916ea011b4b1e15aa
Christabellecp <christabellepabalan@gmail.com>
2021-06-14 16:46:38 -0700

    fixed python files

--- code/app/preprocess.py
+++ code/app/preprocess.py
@@ -23,7 +23,7 @@ def get_text_from_pdf(path):
     text = ''.join(map(lambda x: pytesseract
                        .image_to_string(x).replace('-\n', ''), pages))
     return text
-
+    
 
 def return_flagged(pdf_path, df_cal):
     text = return_text(pdf_path)
@@ -32,34 +32,31 @@ def return_flagged(pdf_path, df_cal):
 
     # Create keyword lists
     q3_keys = ["falling", "fall", "fell", "tripped", "trip", "tumble"]
-    q4_keys = ["depression", "mental", "bipolar", "sad", "upset",
-               "trauma", "traumatic"]
+    q4_keys = ["depression", "mental", "bipolar", "sad", "upset", "trauma", "traumatic"]
     q5_keys = ["flu"]
     q6_keys = ["pneumonia", "pneumococcal"]
     q7_keys = ["diabetes", "foot"]
     q8_keys = ["walk", "walking", "moving"]
     q9_keys = ["bed"]
     q10_keys = ["bathing", "bathe", "bath"]
-    q11_keys = ["breath", "shortness of breath", "shortness",
-                "breathing", "breathe", "oxygen"]
-    q12_keys = ["wound", "wounds", "cut", "slice", "lesion",
-                "gash", "puncture", "slash", "laceration", "tear",
-                "rent", "puncture", "slash", "sore", "graze", "scratch",
+    q11_keys = ["breath", "shortness of breath", "shortness", "breathing", "breathe", "oxygen"]
+    q12_keys = ["wound", "wounds", "cut", "slice", "lesion",\
+                "gash", "puncture", "slash", "laceration", "tear",\
+                "rent", "puncture", "slash", "sore", "graze", "scratch",\
                 "scrape", "abrasion", "bruise", "contusion", "trauma"]
     q16_keys = ["ulcer", "pressure", "skin", "swelling", "post-acute"]
 
     q_keys = [q3_keys, q4_keys, q5_keys, q6_keys, q7_keys, q8_keys,
               q9_keys, q10_keys, q11_keys, q12_keys, q16_keys]
 
-    keys = [f'Q{i}' for i in [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16]]
+    keys = [f'Q{i}' for i in [3,4,5,6,7,8,9,10,11,12,16]]
 
     flags = [any(set(x).intersection(set(word_list))) for x in q_keys]
     flagged_qtopic = dict(zip(keys, flags))
-#     Q_flagged = [Q for (Q, bool_flag) in flagged_qtopic.items() if
-#                  bool_flag] # questions that were flagged by NLP
-#     filtered_qs = [key for key, value in flagged_qtopic.items()
-#     if value]
+#     Q_flagged = [Q for (Q, bool_flag) in flagged_qtopic.items() if bool_flag] # questions that were flagged by NLP
+#     filtered_qs = [key for key, value in flagged_qtopic.items() if value]  
     return flagged_qtopic
+
 
 
 def return_text(pdf_path):
@@ -77,16 +74,12 @@ def return_text(pdf_path):
     # print(f'zipcode: {re.findall(pattern, text)}')
     # zipcode = re.findall(pattern, text)
 
+
     # Remove other Digits
     translation_dig = {ord(c): '' for c in string.digits}
     text = text.translate(translation_dig)
     return text
-<<<<<<< HEAD
-
-
-    
    
- 
 def text_process(s):
     """Text-Preprocessing"""
     nlp = en_core_web_sm.load()
@@ -97,5 +90,3 @@ def text_process(s):
             continue
         filtered_list.append(token.text.lower())
     return filtered_list
-=======
->>>>>>> 9c69a220060fa7c3da524ddbb55921007d3bf500
