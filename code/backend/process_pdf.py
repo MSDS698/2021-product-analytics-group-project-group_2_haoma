d4a901e6b305d55f609c368295fd0cea609c3e43
Christabellecp <christabellepabalan@gmail.com>
2021-06-13 21:54:20 -0700

    process pdf functions

--- code/backend/process_pdf.py
+++ code/backend/process_pdf.py
@@ -11,6 +11,7 @@ from pdf2image import convert_from_path
 import pytesseract
 import spacy
 import re
+from pipeline import *
 
 
 pdf_path = 'referral_example.pdf'
@@ -24,22 +25,64 @@ def get_text_from_pdf(path):
                        .image_to_string(x).replace('-\n', ''), pages))
     return text
 
+def return_flag(zipcode):
+    df_cal = load_df(zipcode)
+    flagged_qtopic = return_flagged(return_text(pdf_path), df_cal)
+    
 
-text = get_text_from_pdf(pdf_path)
+def return_flagged(pdf_path, df_cal):
+    text = return_text(pdf_path)
+    word_list = text_process(text)
+    df_cal = renamed_qcols(df_cal)
 
-# Initalize ordinal translations
-translation_ws = {ord(c): ' ' for c in string.whitespace}
-translation_punc = {ord(c): '' for c in string.punctuation}
+    # Create keyword lists
+    q3_keys = ["falling", "fall", "fell", "tripped", "trip", "tumble"]
+    q4_keys = ["depression", "mental", "bipolar", "sad", "upset", "trauma", "traumatic"]
+    q5_keys = ["flu"]
+    q6_keys = ["pneumonia", "pneumococcal"]
+    q7_keys = ["diabetes", "foot"]
+    q8_keys = ["walk", "walking", "moving"]
+    q9_keys = ["bed"]
+    q10_keys = ["bathing", "bathe", "bath"]
+    q11_keys = ["breath", "shortness of breath", "shortness", "breathing", "breathe", "oxygen"]
+    q12_keys = ["wound", "wounds", "cut", "slice", "lesion",\
+                "gash", "puncture", "slash", "laceration", "tear",\
+                "rent", "puncture", "slash", "sore", "graze", "scratch",\
+                "scrape", "abrasion", "bruise", "contusion", "trauma"]
+    q16_keys = ["ulcer", "pressure", "skin", "swelling", "post-acute"]
 
-# Translate/ clean text
-text = text.translate(translation_ws)
-text = text.translate(translation_punc)
-text = ' '.join(text.split())
-# pattern = re.compile(r'.*(9\d{4})')
-# print(f'zipcode: {re.findall(pattern, text)}')
-# zipcode = re.findall(pattern, text)
+    q_keys = [q3_keys, q4_keys, q5_keys, q6_keys, q7_keys, q8_keys,
+              q9_keys, q10_keys, q11_keys, q12_keys, q16_keys]
+
+    keys = [f'Q{i}' for i in [3,4,5,6,7,8,9,10,11,12,16]]
+
+    flags = [any(set(x).intersection(set(word_list))) for x in q_keys]
+    flagged_qtopic = dict(zip(keys, flags))
+#     Q_flagged = [Q for (Q, bool_flag) in flagged_qtopic.items() if bool_flag] # questions that were flagged by NLP
+#     filtered_qs = [key for key, value in flagged_qtopic.items() if value]  
+    return flagged_qtopic
 
 
-# Remove other Digits
-translation_dig = {ord(c): '' for c in string.digits}
-text = text.translate(translation_dig)
+
+def return_text(pdf_path):
+    text = get_text_from_pdf(pdf_path)
+
+    # Initalize ordinal translations
+    translation_ws = {ord(c): ' ' for c in string.whitespace}
+    translation_punc = {ord(c): '' for c in string.punctuation}
+
+    # Translate/ clean text
+    text = text.translate(translation_ws)
+    text = text.translate(translation_punc)
+    text = ' '.join(text.split())
+    # pattern = re.compile(r'.*(9\d{4})')
+    # print(f'zipcode: {re.findall(pattern, text)}')
+    # zipcode = re.findall(pattern, text)
+
+
+    # Remove other Digits
+    translation_dig = {ord(c): '' for c in string.digits}
+    text = text.translate(translation_dig)
+    return text
+
+
