8ccea1f7839f42996fb5d9673c6bc2dc8d89b2b4
Noah Matsuyoshi <nmats@uw.edu>
2021-06-14 20:33:28 -0700

    change instance type used for eb

--- eb_deploy.sh
+++ eb_deploy.sh
@@ -1,2 +1,2 @@
 eb init group-2-haoma-app -p Docker -r us-west-2 --keyname=$PEM_NAME
-eb create group-2-haoma-env --envvar AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvar AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+eb create group-2-haoma-env --instance_type t2.large --envvar AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvar AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
