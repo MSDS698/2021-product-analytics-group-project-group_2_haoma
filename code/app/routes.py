710e3539e2bb6904573bcf1ad42b331788349724
Christabellecp <christabellepabalan@gmail.com>
2021-06-23 21:44:00 -0700

    added dashboard connection to /patient

--- code/app/routes.py
+++ code/app/routes.py
@@ -178,28 +178,12 @@ def agency():
                            patients=patients)
 
 
+
 @app.route('/patient', methods=['GET', 'POST'])
 @login_required
 def patient():
     "Discharge planner's patient-recommendations dashboard"
-    if request.method == "POST":
-        if len(list(request.form.keys())) != 4:
-            print('Error with number of agencies selected')
-            # flash('Please Select 3 Agencies', "error")
-            # render_template('patient.html',
-            #                loggedin=current_user.is_authenticated,
-            #                patient=patient,
-            #                columns=df_rec.columns,
-            #                data_available=df_available.values,
-            #                data_requested=df_requested.values,
-            #                data_confirmed=df_confirmed.values,
-            #                data_denied=df_denied.values,
-            #                data_removed=df_removed.values,
-            #                specific_services=specific_services)
-        else:
-            resulting_agencies = list(request.form.keys())[:-1]
-            print(resulting_agencies)
-            ### CHRISTABELLE
+    
 
     if(current_user.account_type != "discharge planner"): abort(401)
     id = request.args.get('id', type=int)
@@ -211,9 +195,88 @@ def patient():
     if(os.path.exists(f'code/app/upload_temp/recs/{patient.id}')):
         df_rec = pd.read_pickle(f'code/app/upload_temp/recs/{patient.id}')
     else:
-        _, df_rec = recommender_instance.recommend(df,
+        df, df_rec = recommender_instance.recommend(df,
                                                 patient.boolservices,
                                                 patient.path)
+        
+    if request.method == "POST":
+        if len(list(request.form.keys())) != 4:
+            print('Error with number of agencies selected')
+        else:
+            resulting_agencies = list(request.form.keys())[:-1]
+            agency_df = df
+            agency_df['preventable_readmission'] = round(agency_df['preventable_readmission']/2,2)
+            df3 = agency_df[agency_df.name.isin(resulting_agencies)]
+            patient_name = 'ENTER NAME'
+
+            names = df3.name.tolist()
+            name1 = names[0]
+            name2 = names[1]
+            name3 = names[2]
+
+            daily_activities = ['name', 'better_bathing', 
+                                'better_getting_in_bed', 'better_moving']
+
+            treating_symptoms = ['name', 'improve_breathing',
+                                 'improve_wounds', 'changed_skin']
+
+            # daily activities
+            daily1 = df3[daily_activities].iloc[2].values[1:].tolist()
+            daily2 = df3[daily_activities].iloc[1].values[1:].tolist()
+            daily3 = df3[:3][daily_activities].iloc[0].values[1:].tolist()
+
+            # daily activities
+            symp1 = df3[treating_symptoms].iloc[2].values[1:].tolist()
+            symp2 = df3[treating_symptoms].iloc[1].values[1:].tolist()
+            symp3 = df3[treating_symptoms].iloc[0].values[1:].tolist()
+
+            # preventing harm
+            fall1, fall2, fall3 = funcs.return_agency_data(df3, 'checked_falling')
+            depr1, depr2, depr3 = funcs.return_agency_data(df3, 'checked_depression')
+            pneu1, pneu2, pneu3 = funcs.return_agency_data(df3, 'checked_pneumonia')
+            flu1, flu2, flu3 = funcs.return_agency_data(df3, 'checked_flu')
+
+            # teaching patients
+            med1, med2, med3 = funcs.return_agency_data(df3, 'taught_meds')
+            tmed1, tmed2, tmed3 = funcs.return_agency_data(df3, 'timely_address_meds')
+            diab1, diab2, diab3 = funcs.return_agency_data(df3, 'diabetes_foot')
+
+            # general
+            score1, score2, score3 = funcs.return_agency_data(df3, 'score')
+            time1, time2, time3 = funcs.return_agency_data(df3, 'timely_manner')
+            ppr1, ppr2, ppr3 = funcs.return_agency_data(df3, 'preventable_readmission')
+            dtc1, dtc2, dtc3 = funcs.return_agency_data(df3, 'discharge_community')
+            er1, er2, er3 = funcs.return_agency_data(df3, 'ER')
+            adm1, adm2, adm3 = funcs.return_agency_data(df3, 'readmitted')
+
+            return render_template("dashboard.html", 
+                                   patient_name = patient_name,
+                                   agency_names = json.dumps(names),
+                                   all_scores = json.dumps(df3.score.tolist()),
+                                   daily1 = json.dumps(daily1), daily2 = json.dumps(daily2),
+                                   daily3 = json.dumps(daily3), name1 = json.dumps(name1),
+                                   name2 = json.dumps(name2), name3 = json.dumps(name3),
+                                   symp1 = json.dumps(symp1), symp2 = json.dumps(symp2),
+                                   symp3 = json.dumps(symp3), score1 = json.dumps(score1),
+                                   score2 = json.dumps(score2), score3 = json.dumps(score3),
+                                   time1 = json.dumps(time1), time2 = json.dumps(time2),
+                                   time3 = json.dumps(time3), fall1 = json.dumps(fall1),
+                                   fall2 = json.dumps(fall2), fall3 = json.dumps(fall3),
+                                   depr1 = json.dumps(depr1), depr2 = json.dumps(depr2),
+                                   depr3 = json.dumps(depr3), pneu1 = json.dumps(pneu1),
+                                   pneu2 = json.dumps(pneu2), pneu3 = json.dumps(pneu3),
+                                   flu1 = json.dumps(flu1), flu2 = json.dumps(flu2),
+                                   flu3 = json.dumps(flu3), med1 = json.dumps(med1),
+                                   med2 = json.dumps(med2), med3 = json.dumps(med3),
+                                   tmed1 = json.dumps(tmed1), tmed2 = json.dumps(tmed2),
+                                   tmed3 = json.dumps(tmed3), diab1 = json.dumps(diab1),
+                                   diab2 = json.dumps(diab2), diab3 = json.dumps(diab3),
+                                   dtc1 = json.dumps(dtc1), dtc2 = json.dumps(dtc2),
+                                   dtc3 = json.dumps(dtc3), er1 = json.dumps(er1),
+                                   er2 = json.dumps(er2), er3 = json.dumps(er3),
+                                   adm1 = json.dumps(adm1), adm2 = json.dumps(adm2),
+                                   adm3 = json.dumps(adm3), ppr1 = json.dumps(ppr1),
+                                   ppr2 = json.dumps(ppr2), ppr3 = json.dumps(ppr3))
 
     rec_status = patient.rec_status
     df_available, df_requested, df_confirmed, df_denied, df_removed = df_rec.loc[[e == "A" for e in rec_status]],\
